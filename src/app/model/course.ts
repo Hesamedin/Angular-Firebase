2966678a9137ba7f65399422ce291e6cbceda508
Hesam Kamalan <hesam.kamalan@gmail.com>
2021-12-30 11:47:51 -0700

    Implementation of the Home screen is done.

--- src/app/model/course.ts
+++ src/app/model/course.ts
@@ -2,15 +2,15 @@ import firebase from 'firebase/app';
 import Timestamp = firebase.firestore.Timestamp;
 
 export interface Course {
-  id: string;
-  description: string;
-  url:string;
-  longDescription: string;
-  iconUrl: string;
-  seqNo:number;
-  categories: string[];
-  lessonsCount: number;
-  promo:boolean;
-  promoStartAt: Timestamp;
+    id: string;
+    description: string;
+    url: string;
+    longDescription: string;
+    iconUrl: string;
+    seqNo: number;
+    categories: string[];
+    lessonsCount: number;
+    promo: boolean;
+    promoStartAt: Timestamp;
 }
 
